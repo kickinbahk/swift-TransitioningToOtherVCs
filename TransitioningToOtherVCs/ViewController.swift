b6de8f7e4bc37e336c0e03a4e08cfa409b4284d3
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-13 07:04:01 -0700

    add tableview and load data

--- TransitioningToOtherVCs/ViewController.swift
+++ TransitioningToOtherVCs/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var checklist = ["Subclas UITableViewDatasource",
+                   "Create a prototype cell",
+                   "Set a unique reuse identifier",
+                   "Wire the tableView to the ViewController",
+                   "Add number of rows",
+                   "Add cellForRowAt"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return checklist.count
   }
-
-
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = checklist[indexPath.row]
+    
+    return cell
+  }
 }
 
