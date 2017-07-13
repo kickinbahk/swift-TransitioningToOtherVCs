8720f7ea412af11151008903036fe108371fd8da
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-07-13 07:36:56 -0700

    add transition from NavVC programatically

--- TransitioningToOtherVCs/NavVC.swift
+++ TransitioningToOtherVCs/NavVC.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class NavVC: UIViewController {
+  var checklist = ["Subclas UITableViewDatasource",
+                   "Create a prototype cell",
+                   "Set a unique reuse identifier",
+                   "Wire the tableView to the ViewController",
+                   "Add number of rows",
+                   "Add cellForRowAt"]
   
 }
 
 extension NavVC: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return checklist.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    let cell = UITableViewCell()
+    
+    cell.textLabel?.text = checklist[indexPath.row]
+    
+    return cell
+  }
+}
+
+extension NavVC: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    
+    let vc = storyboard.instantiateViewController(withIdentifier: "detail")
+    
+    navigationController?.pushViewController(vc, animated: true)
   }
 }
